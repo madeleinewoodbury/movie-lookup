10d3b9875be407fa8331fa91d0a0c483788d7d83
madeleinewoodbury <randine25@gmail.com>
2020-03-16 16:21:22 -0500

    Add initial styling

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className="navbar bg-primary">
       <h1>
         <i className="fas fa-film"></i> Movie Lookup
       </h1>
