184bb872e372c60f693742ecc01154bfba8b6161
madeleinewoodbury <randine25@gmail.com>
2020-03-16 15:59:50 -0500

    Add inital components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Movie Lookup</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>
+        <i className="fas fa-film"></i> Movie Lookup
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
